70c68bab6a7bbf3df4c65beda6ef7289182d1320
Pascal Reichert <pascal@bredex.de>
2025-08-14 10:19:55 +0200

    init spring ai demo project

---
+++ src/main/java/de/bredex/spring_ai_demo/service/OpenAIService.java
@@ -0,0 +1,86 @@
+package de.bredex.spring_ai_demo.service;
+
+import org.springframework.ai.audio.transcription.AudioTranscriptionPrompt;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.content.Media;
+import org.springframework.ai.embedding.EmbeddingModel;
+import org.springframework.ai.image.Image;
+import org.springframework.ai.image.ImageModel;
+import org.springframework.ai.image.ImagePrompt;
+import org.springframework.ai.openai.OpenAiAudioSpeechOptions;
+import org.springframework.ai.openai.OpenAiAudioTranscriptionModel;
+import org.springframework.ai.openai.OpenAiAudioTranscriptionOptions;
+import org.springframework.ai.openai.OpenAiImageOptions;
+import org.springframework.ai.openai.api.OpenAiAudioApi;
+import org.springframework.ai.openai.audio.speech.Speech;
+import org.springframework.ai.openai.audio.speech.SpeechModel;
+import org.springframework.ai.openai.audio.speech.SpeechPrompt;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OpenAIService {
+    private final ChatClient chatClient;
+    private final ImageModel imageModel;
+    private final SpeechModel speechModel;
+    private final OpenAiAudioTranscriptionModel transcriptionModel;
+    private final EmbeddingModel embeddingModel;
+
+    public OpenAIService(ChatClient.Builder builder, ImageModel imageModel, SpeechModel speechModel, OpenAiAudioTranscriptionModel transcriptionModel, EmbeddingModel embeddingModel) {
+        this.chatClient = builder.build();
+        this.imageModel = imageModel;
+        this.speechModel = speechModel;
+        this.transcriptionModel = transcriptionModel;
+        this.embeddingModel = embeddingModel;
+    }
+
+    public String generateText(String prompt) {
+        return this.chatClient.prompt(prompt).call().content();
+    }
+
+    public Image generateImage(String prompt) {
+        return this.imageModel.call(new ImagePrompt(prompt,
+                OpenAiImageOptions.builder()
+                        .quality("hd")
+                        .N(1)
+                        .height(1024)
+                        .width(1024)
+                        .build()
+                )
+        ).getResult().getOutput();
+    }
+
+    public boolean catRecognizer(Media image) {
+        final String response = this.chatClient.prompt()
+                .user(userSpec -> userSpec
+                        .text("Decide whether or not there is a cat in the image. If there is a cat in the image return 'true', otherwise 'false'")
+                        .media(image))
+                .call()
+                .content();
+        return Boolean.parseBoolean(response);
+    }
+
+    public Speech generateSpeech(String prompt) {
+        final OpenAiAudioSpeechOptions speechOptions = OpenAiAudioSpeechOptions.builder()
+                .responseFormat(OpenAiAudioApi.SpeechRequest.AudioResponseFormat.MP3)
+                .speed(1.0f)
+                .model(OpenAiAudioApi.TtsModel.TTS_1.value)
+                .build();
+        final SpeechPrompt speechPrompt = new SpeechPrompt(prompt, speechOptions);
+        return this.speechModel.call(speechPrompt).getResult();
+    }
+
+    public String transcribe(Resource audio) {
+        final OpenAiAudioTranscriptionOptions transcriptionOptions = OpenAiAudioTranscriptionOptions.builder()
+                .language("en")
+                .temperature(0f)
+                .responseFormat(OpenAiAudioApi.TranscriptResponseFormat.TEXT)
+                .build();
+        final AudioTranscriptionPrompt transcriptionRequest = new AudioTranscriptionPrompt(audio, transcriptionOptions);
+        return this.transcriptionModel.call(transcriptionRequest).getResult().getOutput();
+    }
+
+    public float[] generateEmbedding(String text) {
+        return this.embeddingModel.embed(text);
+    }
+}
---
+++ src/test/java/de/bredex/spring_ai_demo/service/OpenAIServiceTest.java
@@ -0,0 +1,69 @@
+package de.bredex.spring_ai_demo.service;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.ai.content.Media;
+import org.springframework.ai.image.Image;
+import org.springframework.ai.openai.audio.speech.Speech;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.util.MimeTypeUtils;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.Arrays;
+
+@SpringBootTest
+class OpenAIServiceTest {
+    @Autowired
+    private OpenAIService service;
+
+    @Disabled
+    @Test
+    void testGenerateText() {
+        final String response = this.service.generateText("Hello");
+        System.out.printf("AI: %s%n", response);
+    }
+    @Disabled
+    @Test
+    void testGenerateImage() {
+        final Image image = this.service.generateImage("A cat holding an ice cream");
+        System.out.printf("Generated image of a cat: %s%n", image.getUrl());
+    }
+    @Disabled
+    @Test
+    void testCatRecognizer() {
+        final Media image = new Media(MimeTypeUtils.IMAGE_JPEG, new ClassPathResource("image/cat.jpg"));
+        final boolean containsCat = this.service.catRecognizer(image);
+        System.out.println(containsCat ? "Image shows a cat" : "Image shows not a cat");
+    }
+    @Disabled
+    @Test
+    void testGenerateSpeech() {
+        try {
+            final String prompt = Files.readString(Path.of("src/test/resources/audio/speech.trans"));
+            final Speech response = this.service.generateSpeech(prompt);
+            Files.write(Path.of("src/test/resources/audio/speech.mp3"), response.getOutput(), StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    @Disabled
+    @Test
+    void testTranscribe() {
+        final Resource audio = new FileSystemResource("src/test/resources/audio/speech.flac");
+        final String response = this.service.transcribe(audio);
+        System.out.printf("Audio transcription: %s", response);
+    }
+    @Disabled
+    @Test
+    void testGenerateEmbedding() {
+        final float[] embedding = this.service.generateEmbedding("Hello AI");
+        System.out.printf("Embedding of 'Hello AI': %s%n", Arrays.toString(embedding));
+    }
+}
